ded05dd96bf3812e3930d78492fcf82d522f8c94
Trevor Kilvington <trevorgk@gmail.com>
2019-04-19 20:59:59 +1000

    Complete Type Definitions section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
-import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log('Hello Node.js project.');
+const app = express();
+app.use(cors());
 
-console.log(process.env.MY_SECRET);
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch'
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids'
+  }
+};
+
+const me = users[1];
+
+const schema = gql`
+  type Query {
+    me: User
+    user(id: ID!): User
+    users: [User!]
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: () => {
+      return me;
+    },
+    users: () => {
+      return Object.values(users);
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
